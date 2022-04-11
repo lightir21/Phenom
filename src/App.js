3b510f664da34ae419ee3f19360803080db1f0ab
Meir Meir <lightir21@gmail.com>
2022-04-12 02:17:27 +0300

    a few setups changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React, { useState, useEffect } from "react";
+import data from "./employees.json";
 
 import "./App.css";
 
 function App() {
-  const [employess, setEmployees] = useState([]);
-
-  useEffect(() => {
-    const fetchEmployees = async () => {
-      const res = await fetch("../public/employees.json");
-      console.log(res);
-      const data = await res.json();
-      console.log(data);
-    };
-    fetchEmployees();
-  }, []);
-
+  const [employess, setEmployees] = useState(data);
+  console.log(employess);
   return (
     <div className="App">
       <p>the div</p>
