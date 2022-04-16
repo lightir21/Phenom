d70c46bd8bf9239cd9aefddebbb650aabccb2120
meir <lightir21@gmail.com>
2022-04-16 17:23:17 +0300

    more css tweaking

--- src/components/employeeContainer/EmployeeContainer.jsx
+++ src/components/employeeContainer/EmployeeContainer.jsx
@@ -9,35 +9,46 @@ const styles = {
 const EmployeeContainer = ({ employeeCards, index, style }) => {
   console.log(employeeCards.length);
   return (
-    <div className={`${styles[style]}`}>
-      <hr
-        className={`home__${styles[style]}Line`}
-        style={{ height: `${employeeCards.length * 35 - 35}rem` }}
-      />
-      {employeeCards.map(
-        ({
-          employee_name,
-          business_title,
-          region,
-          profile_pic,
-          subordinates,
-          department,
-        }) => {
-          return (
-            <>
-              <EmployeeCard
-                name={employee_name}
-                role={business_title}
-                locationText={region}
-                profileImage={profile_pic}
-                department={department}
-                subordinates={subordinates}
-              />
-            </>
-          );
-        }
+    <>
+      {style === "horizontal" && (
+        <hr
+          className={`home__${styles[style]}Line`}
+          style={{ width: `${employeeCards.length * 35 - 15}rem` }}
+        />
       )}
-    </div>
+
+      <div className={`${styles[style]}`}>
+        {style === "vertical" && (
+          <hr
+            className={`home__${styles[style]}Line`}
+            style={{ height: `${employeeCards.length * 35 - 35}rem` }}
+          />
+        )}
+        {employeeCards.map(
+          ({
+            employee_name,
+            business_title,
+            region,
+            profile_pic,
+            subordinates,
+            department,
+          }) => {
+            return (
+              <>
+                <EmployeeCard
+                  name={employee_name}
+                  role={business_title}
+                  locationText={region}
+                  profileImage={profile_pic}
+                  department={department}
+                  subordinates={subordinates}
+                />
+              </>
+            );
+          }
+        )}
+      </div>
+    </>
   );
 };
 
